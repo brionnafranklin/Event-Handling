9aa97ffed738868ac30fc18219b689621945438f
Brionna Franklin <s198017@students.aie.edu>
2019-09-16 16:27:27 -0500

    added key button exercise

---
+++ Keyboard Input Exercise/ConsoleKeyEventPublisher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyboard_Input_Exercise
+{
+    class ConsoleKeyEventPublisher
+    {
+        public delegate void TKeyPressed(char key);
+
+        //assign an empty lampa func
+        private TKeyPressed KeyPressedEvent = (char key) => { };
+        
+        public void Subscribe(TKeyPressed func)
+        {
+            KeyPressedEvent += func;
+        }
+        public void Update()
+        {
+            if (Console.KeyAvailable)
+            {
+                char keyPressed = Console.ReadKey(true).KeyChar;
+
+                KeyPressedEvent(keyPressed);
+            }
+        }
+    }
+}
---
+++ Keyboard Input Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyboard_Input_Exercise
+{
+    class Program
+    {
+        static bool shouldQuit = false;
+
+        static void MakeBeep(char key)
+        {
+            Console.Beep(key * 50, key * 5);
+        }
+        static void ListenForQuietKey(char key)
+        {
+            if (key == ' ') shouldQuit = true;
+        }
+        static void Main(string[] args)
+        {
+            ConsoleKeyEventPublisher consoleKeyEventPublisher = new ConsoleKeyEventPublisher();
+
+            consoleKeyEventPublisher.Subscribe(MakeBeep);
+            consoleKeyEventPublisher.Subscribe(ListenForQuietKey);
+
+            while (shouldQuit == false)
+            {
+                consoleKeyEventPublisher.Update();
+            }
+        }
+    }
+}
